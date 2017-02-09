0a8f93bccb8ef63b863b3eac0ce2569149ef33a1
gkozak-neducatio <gkozak@n-educatio.pl>
2017-02-09 14:12:12 +0100

    Added layout improvements

--- src/StudentsSearchBundle/Controller/StudentController.php
+++ src/StudentsSearchBundle/Controller/StudentController.php
@@ -4,7 +4,6 @@ namespace StudentsSearchBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
-use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Response;
